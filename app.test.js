5f9a4c9f9a9475fa57cf359bc671b6533f1c7d4e
PedroJimenez73 <pjimenez@qualitycw.com>
2022-01-30 22:06:01 +0100

    add test

--- app.test.js
+++ app.test.js
@@ -3,10 +3,10 @@ const supertest = require("supertest")
 const request = supertest(app)
 
 describe("/test endpoint", () => {
-    it("Debe devolver un 'Hola Mundo'", async () => {
+    test("Debe devolver un 'Hola Mundo'", async () => {
         const response = await request.get("/")
-        expect(response.status).toBe(200)
         const body = JSON.parse(response.text);
+        expect(response.status).toBe(200);
         expect(body.message).toBe("¡Hola mundo!");
     })
 })
